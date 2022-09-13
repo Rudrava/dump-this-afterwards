3430b1d36e7c6ee8bcc103d1be6f0dd1c7f95500
Rudrava Mukherjee <58327905+Rudrava@users.noreply.github.com>
2022-09-13 20:31:53 +0000

    init

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -1,5 +1,9 @@
 package com.example.springboot;
 
+import java.util.*;
+
+import org.json.JSONObject;
+import org.springframework.boot.jackson.JsonObjectSerializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +12,16 @@ public class HelloController {
 
 	@GetMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot!";
+		return "Hello from Spring Boot!";
+	}
+
+	@GetMapping("/json")
+	public JSONObject json() {
+		JSONObject jsonObj = new JSONObject();
+		jsonObj.put("name", "Tom");
+		jsonObj.put("birthday", "1940-02-10");
+		jsonObj.put("age", 76);
+		return jsonObj;
 	}
 
 }
